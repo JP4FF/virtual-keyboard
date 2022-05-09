7dda9b5c5748c9118c10bd3b6a979b97c418a91f
JP4FF <mihaylik967@gmail.com>
2022-05-09 23:11:49 +0300

    feat: implement creation keyboard via JS

---
+++ js/modules/keys-layout.js
@@ -0,0 +1,849 @@
+const KEYS_LAYOUT = [
+  {
+    value: {
+      RU: "]",
+      ENG: "`",
+    },
+    altValue: {
+      RU: "[",
+      ENG: "~",
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 1,
+  },
+  {
+    value: {
+      RU: "1",
+      ENG: "1",
+    },
+    altValue: {
+      RU: "!",
+      ENG: "!",
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 1,
+  },
+  {
+    value: {
+      RU: "2",
+      ENG: "2",
+    },
+    altValue: {
+      RU: '"',
+      ENG: "@",
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 1,
+  },
+  {
+    value: {
+      RU: "3",
+      ENG: "3",
+    },
+    altValue: {
+      RU: "№",
+      ENG: "#",
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 1,
+  },
+  {
+    value: {
+      RU: "4",
+      ENG: "4",
+    },
+    altValue: {
+      RU: "%",
+      ENG: "$",
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 1,
+  },
+  {
+    value: {
+      RU: "5",
+      ENG: "5",
+    },
+    altValue: {
+      RU: ":",
+      ENG: "%",
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 1,
+  },
+  {
+    value: {
+      RU: "6",
+      ENG: "6",
+    },
+    altValue: {
+      RU: ",",
+      ENG: "^",
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 1,
+  },
+  {
+    value: {
+      RU: "7",
+      ENG: "7",
+    },
+    altValue: {
+      RU: ".",
+      ENG: "&",
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 1,
+  },
+  {
+    value: {
+      RU: "8",
+      ENG: "8",
+    },
+    altValue: {
+      RU: ";",
+      ENG: "*",
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 1,
+  },
+  {
+    value: {
+      RU: "9",
+      ENG: "9",
+    },
+    altValue: {
+      RU: "(",
+      ENG: "(",
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 1,
+  },
+  {
+    value: {
+      RU: "0",
+      ENG: "0",
+    },
+    altValue: {
+      RU: ")",
+      ENG: ")",
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 1,
+  },
+  {
+    value: {
+      RU: "-",
+      ENG: "-",
+    },
+    altValue: {
+      RU: "_",
+      ENG: "_",
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 1,
+  },
+  {
+    value: {
+      RU: "=",
+      ENG: "=",
+    },
+    altValue: {
+      RU: "+",
+      ENG: "+",
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 1,
+  },
+  {
+    value: {
+      RU: null,
+      ENG: null,
+    },
+    altValue: {
+      RU: null,
+      ENG: null,
+    },
+    isOptionKey: true,
+    optionName: "backspace",
+    keyRow: 1,
+  },
+  {
+    value: {
+      RU: null,
+      ENG: null,
+    },
+    altValue: {
+      RU: null,
+      ENG: null,
+    },
+    isOptionKey: true,
+    optionName: "tab",
+    keyRow: 2,
+  },
+  {
+    value: {
+      RU: "й",
+      ENG: "q",
+    },
+    altValue: {
+      RU: null,
+      ENG: null,
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 2,
+  },
+  {
+    value: {
+      RU: "ц",
+      ENG: "w",
+    },
+    altValue: {
+      RU: null,
+      ENG: null,
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 2,
+  },
+  {
+    value: {
+      RU: "у",
+      ENG: "e",
+    },
+    altValue: {
+      RU: null,
+      ENG: null,
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 2,
+  },
+  {
+    value: {
+      RU: "к",
+      ENG: "r",
+    },
+    altValue: {
+      RU: null,
+      ENG: null,
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 2,
+  },
+  {
+    value: {
+      RU: "е",
+      ENG: "t",
+    },
+    altValue: {
+      RU: null,
+      ENG: null,
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 2,
+  },
+  {
+    value: {
+      RU: "н",
+      ENG: "y",
+    },
+    altValue: {
+      RU: null,
+      ENG: null,
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 2,
+  },
+  {
+    value: {
+      RU: "г",
+      ENG: "u",
+    },
+    altValue: {
+      RU: null,
+      ENG: null,
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 2,
+  },
+  {
+    value: {
+      RU: "ш",
+      ENG: "i",
+    },
+    altValue: {
+      RU: null,
+      ENG: null,
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 2,
+  },
+  {
+    value: {
+      RU: "щ",
+      ENG: "o",
+    },
+    altValue: {
+      RU: null,
+      ENG: null,
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 2,
+  },
+  {
+    value: {
+      RU: "з",
+      ENG: "p",
+    },
+    altValue: {
+      RU: null,
+      ENG: null,
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 2,
+  },
+  {
+    value: {
+      RU: "х",
+      ENG: "[",
+    },
+    altValue: {
+      RU: null,
+      ENG: "{",
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 2,
+  },
+  {
+    value: {
+      RU: "ъ",
+      ENG: "]",
+    },
+    altValue: {
+      RU: null,
+      ENG: "}",
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 2,
+  },
+  {
+    value: {
+      RU: "ё",
+      ENG: "\\",
+    },
+    altValue: {
+      RU: null,
+      ENG: "|",
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 2,
+  },
+  {
+    value: {
+      RU: null,
+      ENG: null,
+    },
+    altValue: {
+      RU: null,
+      ENG: null,
+    },
+    isOptionKey: true,
+    optionName: "del",
+    keyRow: 2,
+  },
+  {
+    value: {
+      RU: null,
+      ENG: null,
+    },
+    altValue: {
+      RU: null,
+      ENG: null,
+    },
+    isOptionKey: true,
+    optionName: "caps lock",
+    keyRow: 3,
+  },
+  {
+    value: {
+      RU: "ф",
+      ENG: "a",
+    },
+    altValue: {
+      RU: null,
+      ENG: null,
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 3,
+  },
+  {
+    value: {
+      RU: "ы",
+      ENG: "s",
+    },
+    altValue: {
+      RU: null,
+      ENG: null,
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 3,
+  },
+  {
+    value: {
+      RU: "в",
+      ENG: "d",
+    },
+    altValue: {
+      RU: null,
+      ENG: null,
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 3,
+  },
+  {
+    value: {
+      RU: "а",
+      ENG: "f",
+    },
+    altValue: {
+      RU: null,
+      ENG: null,
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 3,
+  },
+  {
+    value: {
+      RU: "п",
+      ENG: "g",
+    },
+    altValue: {
+      RU: null,
+      ENG: null,
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 3,
+  },
+  {
+    value: {
+      RU: "р",
+      ENG: "h",
+    },
+    altValue: {
+      RU: null,
+      ENG: null,
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 3,
+  },
+  {
+    value: {
+      RU: "о",
+      ENG: "j",
+    },
+    altValue: {
+      RU: null,
+      ENG: null,
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 3,
+  },
+  {
+    value: {
+      RU: "л",
+      ENG: "k",
+    },
+    altValue: {
+      RU: null,
+      ENG: null,
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 3,
+  },
+  {
+    value: {
+      RU: "д",
+      ENG: "l",
+    },
+    altValue: {
+      RU: null,
+      ENG: null,
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 3,
+  },
+  {
+    value: {
+      RU: "ж",
+      ENG: ";",
+    },
+    altValue: {
+      RU: null,
+      ENG: ":",
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 3,
+  },
+  {
+    value: {
+      RU: "э",
+      ENG: "'",
+    },
+    altValue: {
+      RU: null,
+      ENG: '"',
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 3,
+  },
+  {
+    value: {
+      RU: null,
+      ENG: null,
+    },
+    altValue: {
+      RU: null,
+      ENG: null,
+    },
+    isOptionKey: true,
+    optionName: "return",
+    keyRow: 3,
+  },
+  {
+    value: {
+      RU: null,
+      ENG: null,
+    },
+    altValue: {
+      RU: null,
+      ENG: null,
+    },
+    isOptionKey: true,
+    optionName: "shift left",
+    keyRow: 4,
+  },
+  {
+    value: {
+      RU: "я",
+      ENG: "z",
+    },
+    altValue: {
+      RU: null,
+      ENG: null,
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 4,
+  },
+  {
+    value: {
+      RU: "ч",
+      ENG: "x",
+    },
+    altValue: {
+      RU: null,
+      ENG: null,
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 4,
+  },
+  {
+    value: {
+      RU: "с",
+      ENG: "c",
+    },
+    altValue: {
+      RU: null,
+      ENG: null,
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 4,
+  },
+  {
+    value: {
+      RU: "м",
+      ENG: "v",
+    },
+    altValue: {
+      RU: null,
+      ENG: null,
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 4,
+  },
+  {
+    value: {
+      RU: "и",
+      ENG: "b",
+    },
+    altValue: {
+      RU: null,
+      ENG: null,
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 4,
+  },
+  {
+    value: {
+      RU: "т",
+      ENG: "n",
+    },
+    altValue: {
+      RU: null,
+      ENG: null,
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 4,
+  },
+  {
+    value: {
+      RU: "ь",
+      ENG: "m",
+    },
+    altValue: {
+      RU: null,
+      ENG: null,
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 4,
+  },
+  {
+    value: {
+      RU: "б",
+      ENG: ",",
+    },
+    altValue: {
+      RU: null,
+      ENG: "<",
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 4,
+  },
+  {
+    value: {
+      RU: "ю",
+      ENG: ".",
+    },
+    altValue: {
+      RU: null,
+      ENG: ">",
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 4,
+  },
+  {
+    value: {
+      RU: "/",
+      ENG: "/",
+    },
+    altValue: {
+      RU: "?",
+      ENG: "?",
+    },
+    isOptionKey: false,
+    optionName: null,
+    keyRow: 4,
+  },
+  {
+    value: {
+      RU: null,
+      ENG: null,
+    },
+    altValue: {
+      RU: null,
+      ENG: null,
+    },
+    isOptionKey: true,
+    optionName: "shift right",
+    keyRow: 4,
+  },
+  {
+    value: {
+      RU: null,
+      ENG: null,
+    },
+    altValue: {
+      RU: null,
+      ENG: null,
+    },
+    isOptionKey: true,
+    optionName: "control",
+    keyRow: 5,
+  },
+  {
+    value: {
+      RU: null,
+      ENG: null,
+    },
+    altValue: {
+      RU: null,
+      ENG: null,
+    },
+    isOptionKey: true,
+    optionName: "option left",
+    keyRow: 5,
+  },
+  {
+    value: {
+      RU: null,
+      ENG: null,
+    },
+    altValue: {
+      RU: null,
+      ENG: null,
+    },
+    isOptionKey: true,
+    optionName: "command left",
+    keyRow: 5,
+  },
+  {
+    value: {
+      RU: null,
+      ENG: null,
+    },
+    altValue: {
+      RU: null,
+      ENG: null,
+    },
+    isOptionKey: true,
+    optionName: "space",
+    keyRow: 5,
+  },
+  {
+    value: {
+      RU: null,
+      ENG: null,
+    },
+    altValue: {
+      RU: null,
+      ENG: null,
+    },
+    isOptionKey: true,
+    optionName: "command right",
+    keyRow: 5,
+  },
+  {
+    value: {
+      RU: null,
+      ENG: null,
+    },
+    altValue: {
+      RU: null,
+      ENG: null,
+    },
+    isOptionKey: true,
+    optionName: "option right",
+    keyRow: 5,
+  },
+  {
+    value: {
+      RU: null,
+      ENG: null,
+    },
+    altValue: {
+      RU: null,
+      ENG: null,
+    },
+    isOptionKey: true,
+    optionName: "arrow top",
+    keyRow: 5,
+  },
+  {
+    value: {
+      RU: null,
+      ENG: null,
+    },
+    altValue: {
+      RU: null,
+      ENG: null,
+    },
+    isOptionKey: true,
+    optionName: "arrow left",
+    keyRow: 5,
+  },
+  {
+    value: {
+      RU: null,
+      ENG: null,
+    },
+    altValue: {
+      RU: null,
+      ENG: null,
+    },
+    isOptionKey: true,
+    optionName: "arrow bottom",
+    keyRow: 5,
+  },
+  {
+    value: {
+      RU: null,
+      ENG: null,
+    },
+    altValue: {
+      RU: null,
+      ENG: null,
+    },
+    isOptionKey: true,
+    optionName: "arrow right",
+    keyRow: 5,
+  },
+  {
+    value: {
+      RU: null,
+      ENG: null,
+    },
+    altValue: {
+      RU: null,
+      ENG: null,
+    },
+    isOptionKey: true,
+    optionName: "help",
+    keyRow: 0,
+  },
+];
+
+export default KEYS_LAYOUT;
